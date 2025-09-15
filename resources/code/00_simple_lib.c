8255dd9f6e1ca78c5bfcb4f7198fe1ab1a525539
Ayowel <Ayowel@users.noreply.github.com>
2025-09-15 10:32:44 +0200

    chore(code): Add normalized_radians_list_item function to lib

--- resources/code/00_simple_lib.c
+++ resources/code/00_simple_lib.c
@@ -11,3 +11,15 @@ void hello() {
 // tag::const[]
 const int version = 2;
 // end::const[]
+
+// Returns a list item with value between 0 and 2*PI.
+// This function is only here to ensure that the absence of defines, structs,
+// and typedefs in the lib's symbols is not due to them being unused.
+struct linked_list* normalized_radians_list_item(float degree) {
+    float radians = degree * PI / 180;
+    radians = radians - 2*PI*(int)(radians / 2 / PI);
+    struct linked_list* v = calloc(sizeof(struct linked_list), 1);
+    v->value = ternary(radians > 0, radians, radians + 2 * PI);
+    v->next = NULL;
+    return v;
+}
